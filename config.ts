e0253ae063a7dca9e9f062bc2fd18e423c5576b8
Nicola <nicola.arpino@studio.unibo.it>
2020-12-02 19:13:47 +0100

    fixing port for heroku

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export default  {
-    PORT: 80,
+    PORT:  80,
     HOSTNAME: '0.0.0.0',
 }
 
--- index.ts
+++ index.ts
@@ -4,10 +4,12 @@ import config from './config'
 import router from './routes';
 import { hello } from './controller';
 
+const PORT: number = parseInt(<string>process.env.PORT, 10) || config.PORT;
+
 const app = express();
 app.use(errorHandler());
 
 app.get('/hello',hello);
 
-app.listen(config.PORT,config.HOSTNAME);
+app.listen(PORT,config.HOSTNAME);
 console.log(`Listening at ${config.HOSTNAME}:${config.PORT}`);
