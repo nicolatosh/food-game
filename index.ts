7652520dbda26b1655aeab794d8e57258ff3ad02
Nicola <nicola.arpino@studio.unibo.it>
2020-12-02 19:17:37 +0100

    fixing port for heroku:2

--- index.ts
+++ index.ts
@@ -5,11 +5,12 @@ import router from './routes';
 import { hello } from './controller';
 
 const PORT: number = parseInt(<string>process.env.PORT, 10) || config.PORT;
+const HOSTNAME = "food-game.herokuapp.com";
 
 const app = express();
 app.use(errorHandler());
 
 app.get('/hello',hello);
 
-app.listen(PORT,config.HOSTNAME);
+app.listen(PORT,HOSTNAME);
 console.log(`Listening at ${config.HOSTNAME}:${config.PORT}`);
