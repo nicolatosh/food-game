5743990cca564e12f32dfca1fb66e1332ba06751
Nicola Arpino <nicola.arpino@studio.unibo.it>
2021-02-12 09:53:57 +0100

    fixed url bug in matchservice

--- food_game_api/config.ts
+++ food_game_api/config.ts
@@ -2,8 +2,8 @@ export default  {
     PORT:  80,
     HOSTNAME: '0.0.0.0',
     BACKEND: process.env.BACKEND || "localhost",
-    MATCH_SERVICE_URL: 'http://' + process.env.BACKEND + ':8080',
-    USER_SERVICE_URL: 'http://' + process.env.BACKEND + ':5001'
+    MATCH_SERVICE_URL: `http://${process.env.BACKEND}:8080`,
+    USER_SERVICE_URL: `http://${process.env.BACKEND}:5001`,
 }
 
 
--- matchservice/src/main/java/com/matchservice/matchservice/config/DataSourceRoutes.java
+++ matchservice/src/main/java/com/matchservice/matchservice/config/DataSourceRoutes.java
@@ -1,7 +1,7 @@
 package com.matchservice.matchservice.config;
 
 public class DataSourceRoutes {
-    private static final String ADAPTER_URL = "0.0.0.0:";
+    private static final String ADAPTER_URL = "http://0.0.0.0:";
     private static final String ADAPTER_PORT = "5000";
     public static final String INGREDIENTS_URL = ADAPTER_URL + ADAPTER_PORT + "/ingredients";
     public static final String RECIPES_URL = ADAPTER_URL + ADAPTER_PORT + "/recipes";
